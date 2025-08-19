81c49cb2ffc7a144cafbee973577e1717be1511b
Jeffry Gonzalez <jeff@hypertheory.com>
2025-08-19 14:07:02 -0400

    Auto-commit changes before creating step 03-moving-to-model

--- src/articles/components/list-sort-prefs.ts
+++ src/articles/components/list-sort-prefs.ts
@@ -3,6 +3,7 @@ import {
   ChangeDetectionStrategy,
   signal,
   output,
+  model,
 } from '@angular/core';
 import { ArticleSortOptions } from '../types';
 
@@ -13,14 +14,14 @@ import { ArticleSortOptions } from '../types';
   template: ` <div class="join">
     <button
       [disabled]="sortOption() === 'oldestFirst'"
-      (click)="changeSortOrder('oldestFirst')"
+      (click)="sortOption.set('oldestFirst')"
       class="btn join-item"
     >
       Oldest First
     </button>
     <button
       [disabled]="sortOption() === 'newestFirst'"
-      (click)="changeSortOrder('newestFirst')"
+      (click)="sortOption.set('newestFirst')"
       class="btn join-item"
     >
       Newest First
@@ -29,11 +30,5 @@ import { ArticleSortOptions } from '../types';
   styles: ``,
 })
 export class ListSortPrefs {
-  sortOption = signal<ArticleSortOptions>('oldestFirst');
-  sortChanged = output<ArticleSortOptions>();
-
-  changeSortOrder(by: ArticleSortOptions) {
-    this.sortOption.set(by);
-    this.sortChanged.emit(by); // send  a message to the parent component that this happened.
-  }
+  sortOption = model<ArticleSortOptions>('oldestFirst'); // both an input and output
 }
