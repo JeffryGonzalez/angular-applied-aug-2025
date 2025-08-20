31c065b556420279b5ae9d7315aad764d27f7afd
Jeffry Gonzalez <jeff@hypertheory.com>
2025-08-20 15:25:19 -0400

    03-added-component-input-binding

---
+++ breakpoints/articles-state/03-added-component-input-binding/src/app/app.config.ts
@@ -0,0 +1,25 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withComponentInputBinding,
+  withPreloading,
+  withViewTransitions,
+} from '@angular/router';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app.routes';
+
+// this is the "global"
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withComponentInputBinding(), // if I have a route parameter and matching -named input, just do it for me.
+      withPreloading(PreloadAllModules),
+      withViewTransitions(),
+    ),
+    provideHttpClient(withFetch()),
+  ],
+};
