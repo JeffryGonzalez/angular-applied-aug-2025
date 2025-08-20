cdb8056545f78949dc99468345ac9bf98301b19e
Jeffry Gonzalez <jeff@hypertheory.com>
2025-08-20 15:20:19 -0400

    Auto-commit changes before creating step 02-added-routing

--- src/articles/articles.routes.ts
+++ src/articles/articles.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Articles } from './articles';
 import { List } from './pages/list';
 import { Prefs } from './pages/prefs';
+import { Details } from './pages/details';
 export const ARTICLES_ROUTES: Routes = [
   {
     path: '',
@@ -14,6 +15,10 @@ export const ARTICLES_ROUTES: Routes = [
       {
         path: 'prefs',
         component: Prefs,
+      },
+      {
+        path: 'details/:id',
+        component: Details,
       },
     ],
   },
--- src/articles/components/article-list-item.ts
+++ src/articles/components/article-list-item.ts
@@ -1,11 +1,12 @@
 import { Component, ChangeDetectionStrategy, input } from '@angular/core';
 import { ApiArticleItem } from '../types';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-article-list-item',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   template: `
     <div class="card card-border bg-base-100 w-96">
       <div class="card-body">
@@ -13,6 +14,9 @@ import { DatePipe } from '@angular/common';
         <p>{{ article().description }}</p>
         <p>{{ article().added | date }}</p>
         <div class="card-actions justify-end">
+          <a [routerLink]="['details', article().id]" class="btn btn-primary"
+            >Details</a
+          >
           <a [href]="article().link" target="_blank" class="btn btn-primary"
             >Visit</a
           >
