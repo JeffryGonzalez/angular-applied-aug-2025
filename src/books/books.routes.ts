9c95c08b32cfeb988b081b55bce8d7e6436297d8
NAthan Muennich <nmbitnami@gmail.com>
2025-08-21 20:05:42 +0000

    added stats

--- src/books/books.routes.ts
+++ src/books/books.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { Books } from './books';
 import { List } from './pages/list';
+import { Stats } from './pages/stats';
 export const BOOKS_ROUTES: Routes = [
   {
     path: '',
     component: Books,
     children: [
       {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'list',
+      },
+      {
         path: 'list',
         component: List,
+      },
+      {
+        path: 'stats',
+        component: Stats,
       },
       {
         path: '**',
