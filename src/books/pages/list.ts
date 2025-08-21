f1e36a39a1c2535a779f9f4305dd74e346435c4c
Mike Hanna <mikiemike83@gmail.com>
2025-08-21 19:17:29 +0000

    Sprint 1: change display of json to table

--- src/books/pages/list.ts
+++ src/books/pages/list.ts
@@ -1,16 +1,43 @@
-import { JsonPipe } from '@angular/common';
+// import { JsonPipe } from '@angular/common';
 import { Component, ChangeDetectionStrategy, resource } from '@angular/core';
 
 @Component({
   selector: 'app-books-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [JsonPipe],
+  imports: [],
   template: `
     <p>Books List</p>
+    <div class="h-150 overflow-x-auto">
+      <table class="table table-xs table-pin-rows table-pin-cols">
+        <thead>
+          <tr>
+            <th></th>
+            <th>TITLE</th>
+            <th>AUTHOR</th>
+            <th>YEAR</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for (book of books.value(); track book.id) {
+            <tr>
+              <th>{{ book.id }}</th>
+              <td>{{ book.title }}</td>
+              <td>{{ book.author }}</td>
+              <td>{{ book.year }}</td>
+            </tr>
+          }
+        </tbody>
+        <tfoot>
+          <tr></tr>
+        </tfoot>
+      </table>
+    </div>
 
-    <pre>
+    <br />
+
+    <!-- <pre>
     {{ books.value() | json }}
-</pre>
+</pre> -->
   `,
   styles: ``,
 })
