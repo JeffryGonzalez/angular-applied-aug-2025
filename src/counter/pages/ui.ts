fcea04203e20ff4781477720dab9b69b07e418f0
NAthan Muennich <nmbitnami@gmail.com>
2025-08-21 20:35:20 +0000

    remove reset button

--- src/counter/pages/ui.ts
+++ src/counter/pages/ui.ts
@@ -17,7 +17,6 @@ import { CounterStore } from '../stores/counter';
       </button>
       <span>{{ store.current() }}</span>
       <button (click)="store.increment()" class="btn btn-success">+</button>
-      <button>Reset</button>
     </div>
     @if (store.fizzBuzz()) {
       <div class="alert alert-info">
