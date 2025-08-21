c3de3076b72f101a9302da17fd03e389bb256246
Jeffry Gonzalez <jeff@hypertheory.com>
2025-08-21 10:24:43 -0400

    before component

--- src/counter/pages/prefs.ts
+++ src/counter/pages/prefs.ts
@@ -11,21 +11,21 @@ import { CounterStore } from '../stores/counter';
       <button
         (click)="store.setBy(1)"
         [disabled]="store.by() === 1"
-        class="btn btn-success join-item"
+        class="btn btn-primary join-item"
       >
         1
       </button>
       <button
         (click)="store.setBy(3)"
         [disabled]="store.by() === 3"
-        class="btn btn-success join-item"
+        class="btn btn-primary join-item"
       >
         3
       </button>
       <button
         (click)="store.setBy(5)"
         [disabled]="store.by() === 5"
-        class="btn btn-success join-item"
+        class="btn btn-primary join-item"
       >
         5
       </button>
